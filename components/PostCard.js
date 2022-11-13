99468afe8c05be95d1674945fc78032d4d162533
Caridad Rivera <ccrive10@gmail.com>
2022-11-12 23:05:09 -0500

    added attributes to post

--- components/PostCard.js
+++ components/PostCard.js
@@ -6,14 +6,14 @@ export default function PostCard(props) {
 
   return (
     <div className='p-2 border flex items-stretch border-yellow border-solid m-4'>
-       <div className='monserrat-text flex-1 text-white '>
+       <div className='monserrat-text flex text-white '>
            {children}     
         </div> 
-
         <div className='flex items-center text-yellow'>
             <i onClick={()=>  setEdit(true)} className='fa-solid fa-pencil px-2 cursor-pointer'></i>
             <i className='fa-solid fa-trash-can px-2 cursor-pointer'></i>          
-        </div>     
+        </div>    
+         
     </div>
   )
 }
--- components/UserProfile.js
+++ components/UserProfile.js
@@ -9,9 +9,10 @@ export default function UserProfile() {
 
 
   const { userInfo, currentUser} = useAuth()
-  const [addPost, setAddPost] = useState(false);
   const [post, setPost] = useState('')
+  const [topic, setPostTopic] = useState('')
   const [edit, setEdit] = useState(false)
+  const [mirror, setMirror] = useState(0)
   
   const {posts, loading, error, setPosts} = fetchUserPosts()
   
@@ -23,18 +24,26 @@ export default function UserProfile() {
       const newPostKey = Object.keys(posts).length === 0 ? 1 : Math.max(...Object.keys(posts)) + 1
       setPosts({...posts, [newPostKey]: post})
       setPost('')
+      setPostTopic('')
+
       const userRef = doc(db, 'users', currentUser.uid)  
       await setDoc(userRef, {
         'posts': {
-          [newPostKey]: post
+          [newPostKey]: post, 
+          topic: topic, 
+          mirror: mirror
         }
       }, {merge:true})
+
+
+
 
   }
 
  return (
     <div className='monserrat-text left-0 top-0 w-full max-w-[65ch] text-xs sm:text-sm mx-auto flex flex-col '>
-       <input className=' flex flex-wrap box-content h-32 p-10 border-4 sm:text-lg text-slate-900 ' type='text' placeholder='Add post' value={post} onChange={(e)=> setPost(e.target.value)}  />
+       <textarea className=' flex flex-wrap box-content h-10  border-4 sm:text-lg text-slate-900 ' type='text' placeholder='Topic' value={topic} onChange={(e)=>setPostTopic(e.target.value)}  />
+       <textarea className=' flex flex-wrap h-48  border-4  sm:text-lg text-slate-900 ' type='text' placeholder='Add post' value={post} onChange={(e)=> setPost(e.target.value)}  />
           <button onClick={handleAddPost}  className='bg-yellow-300 text-slate-900 py-3 monserrat-text uppercase'> Add</button>
    
           {(userInfo && loading) && (<div className='flex-1'>
@@ -46,7 +55,7 @@ export default function UserProfile() {
             <>
               {Object.keys(posts).map((post, i) => {
                 return(
-                  <PostCard key={i} setEdit={setEdit} edit={edit}>
+                  <PostCard key={i}>
                     {posts[post]}
                   </PostCard>
                 )
